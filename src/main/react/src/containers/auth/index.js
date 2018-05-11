27155daac824e450573f3d129f7d66e584443493
lingjiaju <lingjiaju@qq.com>
2018-05-11 19:04:00 +0800

    增加error页面跳转

--- src/main/react/src/containers/auth/index.js
+++ src/main/react/src/containers/auth/index.js
@@ -28,6 +28,7 @@ class Routes extends Component {
                 <Switch>
                     {this.props.auth.get('routes').has('/login')?<Route exact path='/login' component={Login}/> : ''}
                     {this.props.auth.get('routes').has('/home')?<Route exact path='/home' component={Home}/> : ''}
+                    <Route render={() => <Redirect to="/error"/>} />
                 </Switch>
             )
         }
